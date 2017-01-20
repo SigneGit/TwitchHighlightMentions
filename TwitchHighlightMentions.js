5a2486da87bf2b860173483a63607bacd62a73b2
xzero213 <xzero213@gmail.com>
2017-01-20 09:42:43 -0600

    Add files via upload

--- TwitchHighlightMentions.js
+++ TwitchHighlightMentions.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name        Twitch Highlight Mentions
 // @Author      /u/Signe_
-// @namespace   *.twitch.tv/*
-// @include       https://*.twitch.tv/*
+// @namespace   *.twitch.tv/*/chat
+// @include       https://*.twitch.tv/*/chat
 // @version     1
 // @grant       none
 // ==/UserScript==
 
 //Starts the loop
 var Timer = setInterval(Loop, 1000);
-//How many iteractions the loop as gone through (resets every 60 seconds)
+//How many iteractions the loop has gone through (resets every 60 seconds)
 var Counter = 0;
 //Dictionary containing peoples name and there color
 var Dictionary = [];
@@ -21,15 +21,19 @@ function Loop() {
       //No reason to grab the jtv bots color
       if (From[i].innerText != 'jtv') {
         //Adds that persons name and style color to the dictionary
-        AddToDict('@' + From[i].innerText.toLowerCase(), From[i].getAttribute('style'));
+        AddToDict('@' + From[i].innerText, From[i].getAttribute('style'));
       }
     }
     var UserMention = document.getElementsByClassName('user-mention'); //Grabs all the user mentions
     for (var j = 0; j < UserMention.length; j++) {
       //Sets the color of the mention to be that persons color in chat
       UserMention[j].setAttribute('style', ReturnFromDict(UserMention[j].innerText.toLowerCase()));
+      if(DictContains('*' + UserMention[j].innerText.toLowerCase())){
+        //Sets the persons name to be the correct case (@example would become @Example if that was there name)
+        UserMention[j].innerText = ReturnFromDict('*' + UserMention[j].innerText.toLowerCase());
+      }
     }
-  } 
+  }
   else {
     if(Counter > 8){
       Counter = 0;
@@ -40,28 +44,32 @@ function Loop() {
   if (Counter > 60) {
     // clear Counter and Dictionary after 60 seconds
     Counter = 0;
-    Dictionary = [
-    ];
+    Dictionary = [];
   }
 }
-function ReturnFromDict(key) {
+function ReturnFromDict(Key) {
   //Returns a value based on the key if it doesn't exist return nothing
-  if (DictContains(key)) {
-    return Dictionary[key];
+  if (DictContains(Key)) {
+    return Dictionary[Key];
   } else {
     return '';
   }
 }
 function AddToDict(Key, Value) {
   //Adds a key value pair to the dictionary
-  if (!DictContains(Key))
-  Dictionary[Key] = Value;
+  var _key = Key.toLowerCase();
+  if (!DictContains(_key)){
+    Dictionary[_key] = Value;
+  }
+  if(!DictContains('*' + _key)){
+    Dictionary['*' + _key] = Key;
+  }
 }
 function DictContains(Key) {
   //Checks if the Dictionary conains a key
   if (Dictionary[Key] == undefined) {
     return false;
-  } 
+  }
   else {
     return true;
   }
